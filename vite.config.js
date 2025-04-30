6203aa52107ac14a1753f90227b34d2ff34070f1
sayyidan-i <sayyidan2020@gmail.com>
2025-04-30 19:35:14 +0700

    try to solve vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        // SVGR options
+        exportType: 'default',
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: '**/*.svg',
+    }),
+  ],
+  build: {
+    assetsInlineLimit: 0, // This prevents inlining SVGs as base64
+  }
+});
