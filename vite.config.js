d2430d063c308bc46592a26ecd209b44faa1b588
sayyidan-i <sayyidan2020@gmail.com>
2025-04-30 19:43:24 +0700

    back

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        // SVGR options
-        exportType: 'default',
-        ref: true,
-        svgo: false,
-        titleProp: true,
-      },
-      include: '**/*.svg',
-    }),
-  ],
-  build: {
-    assetsInlineLimit: 0, // This prevents inlining SVGs as base64
-  }
-});
+  plugins: [react()],
+})
